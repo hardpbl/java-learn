2ad93b0666a5d2ae234927af71ba540be2fa294e
20811 <bailiang.pan@hand-china.com>
2021-04-05 23:49:54 +0800

    [add] 新增操作数栈

---
+++ jvm/src/main/java/basic/day04/OperandStackDemo.java
@@ -0,0 +1,14 @@
+package basic.day04;
+
+/**
+ * @author panbailiang
+ * @Classname OperandStackDemo
+ * @Date 2021/4/5 11:04 下午
+ */
+public class OperandStackDemo {
+    public void testAddOperation(){
+        byte i =15;
+        int j = 8;
+        int k = i + j;
+    }
+}
