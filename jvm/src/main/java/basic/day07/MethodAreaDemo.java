bdbd881692cffc2d61c8623487152b779f3abb40
20811 <bailiang.pan@hand-china.com>
2021-04-08 22:36:49 +0800

    [add] 新增方法区

---
+++ jvm/src/main/java/basic/day07/MethodAreaDemo.java
@@ -0,0 +1,16 @@
+package basic.day07;
+
+/**
+ * @author panbailiang
+ * @Classname MethodAreaDemo
+ * @Date 2021/4/8 8:48 下午
+ */
+public class MethodAreaDemo {
+    public static void main(String[] args) {
+        int x = 500;
+        int y = 100;
+        int a = x / y;
+        int b = 50;
+        System.out.println(a + b);
+    }
+}
