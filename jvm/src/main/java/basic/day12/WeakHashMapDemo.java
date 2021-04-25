09e5e0ab0ee29e8efb7d57872c29204841fd5d54
20811 <bailiang.pan@hand-china.com>
2021-04-25 23:56:00 +0800

    [add] 新增ThreadLocal

---
+++ jvm/src/main/java/basic/day12/WeakHashMapDemo.java
@@ -0,0 +1,19 @@
+package basic.day12;
+
+import java.util.HashMap;
+import java.util.WeakHashMap;
+
+/**
+ * @author panbailiang
+ * @Classname WeakHashMapDemo
+ * @Date 2021/4/25 7:38 下午
+ */
+public class WeakHashMapDemo {
+    public static void main(String[] args) {
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+
+        WeakHashMap<Object, Object> objectObjectWeakHashMap = new WeakHashMap<>();
+
+
+    }
+}
